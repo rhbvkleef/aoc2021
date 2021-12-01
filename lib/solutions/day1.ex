10a66876cd911a0f90c5cbcf1b33c7d99a8aef34
Rolf  van Kleef <rkleef@iapc.nl>
2021-12-01 19:35:05 +0100

    meta(day1): Remove call socket

--- lib/solutions/day1.ex
+++ lib/solutions/day1.ex
@@ -6,30 +6,24 @@ defmodule Aoc2021.Solution.Day1 do
   def parse(text), do: Aoc2021.integers1d(text)
 
   @impl Aoc2021.Solution
-  @spec part1([integer]) :: non_neg_integer
-  def part1(integers), do: count_increasing(integers)
-
-  @spec count_increasing([integer], non_neg_integer) :: non_neg_integer
-  defp count_increasing(list, count \\ 0)
-  defp count_increasing([a, b | rest], count) when a < b do
-    count_increasing [b | rest], count + 1
+  @spec part1([integer], non_neg_integer) :: non_neg_integer
+  def part1(list, count \\ 0)
+  def part1([a, b | rest], count) when a < b do
+    part1 [b | rest], count + 1
   end
-  defp count_increasing([_a | rest], count) do
-    count_increasing rest, count
+  def part1([_a | rest], count) do
+    part1 rest, count
   end
-  defp count_increasing([], count), do: count
+  def part1([], count), do: count
 
   @impl Aoc2021.Solution
-  @spec part2([integer]) :: non_neg_integer
-  def part2(integers), do: count_sliding(integers)
-
-  @spec count_sliding([integer], non_neg_integer) :: non_neg_integer
-  defp count_sliding(list, count \\ 0)
-  defp count_sliding([a, b, c, d | rest], count) when (a + b + c) < (b + c + d) do
-    count_sliding [b, c, d | rest], count + 1
+  @spec part2([integer], non_neg_integer) :: non_neg_integer
+  def part2(list, count \\ 0)
+  def part2([a, b, c, d | rest], count) when (a + b + c) < (b + c + d) do
+    part2 [b, c, d | rest], count + 1
   end
-  defp count_sliding([_a | rest], count) do
-    count_sliding rest, count
+  def part2([_a | rest], count) do
+    part2 rest, count
   end
-  defp count_sliding(_list, count), do: count
+  def part2(_list, count), do: count
 end
