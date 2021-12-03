5ea551b953669f564f8dcdf833d366ce6ca875d4
Rolf  van Kleef <rkleef@iapc.nl>
2021-12-03 17:15:08 +0100

    meta(day3): Optimizations to both solutions

--- lib/solutions/day3.ex
+++ lib/solutions/day3.ex
@@ -2,8 +2,6 @@ defmodule Aoc2021.Solution.Day3 do
   @behaviour Aoc2021.Solution
 
   @impl Aoc2021.Solution
-  @spec parse(binary) :: [[char]]
-  @doc "Parse the input as a 1d list of charlists"
   defdelegate parse(input), to: Aoc2021, as: :chars2d
 
   @impl Aoc2021.Solution
@@ -11,8 +9,12 @@ defmodule Aoc2021.Solution.Day3 do
     total = length(input)
 
     one_counts = input
-      |> transpose
-      |> Enum.map(fn bit -> Enum.count(bit, fn elem -> elem == ?1 end) end)
+      |> Enum.reduce(List.duplicate(0, length(hd(input))), fn elem, acc ->
+        Enum.zip(elem, acc)
+          |> Enum.map(fn
+            {?1, a} -> a + 1
+            {_, a} -> a end)
+      end)
 
     most = one_counts
       |> Enum.map(fn
@@ -21,47 +23,34 @@ defmodule Aoc2021.Solution.Day3 do
       end)
       |> :erlang.list_to_integer(2)
 
-    mask = one_counts
-      |> Enum.reduce(1, fn _, acc -> Bitwise.|||(Bitwise.<<<(acc, 1), 1) end)
-      |> Bitwise.>>>(1)
+    mask = Enum.reduce(one_counts, 1, fn _, acc -> Bitwise.<<<(acc, 1) end) - 1
 
     most * Bitwise.&&&(Bitwise.bnot(most), mask)
   end
 
-  defp transpose([[] | _]), do: []
-  defp transpose(list), do: [Enum.map(list, &hd/1) | transpose(Enum.map(list, &tl/1))]
-
   @impl Aoc2021.Solution
   def part2 input do
-    entries = input |> Enum.map(fn cur -> {cur, cur} end)
-
-    {oxy, _} = part2_reduce(entries, fn (a, b) -> if a <= b do ?1 else ?0 end end)
-    {co2, _} = part2_reduce(entries, fn (a, b) -> if a <= b do ?0 else ?1 end end)
+    oxy = part2_reduce(input, fn (a, b) -> a > b end)
+    co2 = part2_reduce(input, fn (a, b) -> a <= b end)
 
     :erlang.list_to_integer(oxy, 2) * :erlang.list_to_integer(co2, 2)
   end
 
-  defp part2_reduce [result], _ do
-    result
-  end
-  defp part2_reduce input, comparator do
-    {zeros, ones} = count_column(input)
+  defp part2_reduce(lines, comparator, acc \\ [])
+  defp part2_reduce([line], _, acc), do: Enum.reverse(acc) ++ line
+  defp part2_reduce(lines, comparator, acc) do
+    {zeros, ones} = lines
+      |> Enum.reduce(
+        {0, 0}, fn [chr | _], {zeros, ones} ->
+          if chr == ?0 do {zeros + 1, ones}
+          else {zeros, ones + 1} end
+        end)
 
-    by = apply(comparator, [zeros, ones])
-    result = input
-      |> Enum.filter(fn {_, [ih | _]} -> ih == by end)
-      |> Enum.map(fn {actual, [_ | rest]} -> {actual, rest} end)
+    by = if apply(comparator, [zeros, ones]) do ?0 else ?1 end
+    result = lines
+      |> Enum.filter(fn [ih | _] -> ih == by end)
+      |> Enum.map(&tl/1)
 
-    part2_reduce  result, comparator
-  end
-
-  defp count_column pairs do
-    Enum.reduce(
-      pairs,
-      {0, 0},
-      fn {_, [chr | _]}, {zeros, ones} ->
-        if chr == ?0 do {zeros + 1, ones}
-        else {zeros, ones + 1} end
-      end)
+    part2_reduce result, comparator, [by | acc]
   end
 end
