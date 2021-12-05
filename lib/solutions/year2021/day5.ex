85ba50ede57b23ba42cd025af37af2e0bfb5afae
Rolf  van Kleef <rkleef@iapc.nl>
2021-12-05 20:55:57 +0100

    meta(day5): Use ets instead of map

--- lib/solutions/year2021/day5.ex
+++ lib/solutions/year2021/day5.ex
@@ -10,37 +10,55 @@ aoc 2021, 5 do
 
   @impl Aoc.Solution
   def part1(input) do
+    ets = :ets.new(:state_store, [:set, write_concurrency: true, read_concurrency: true])
+
     Enum.filter(input, fn [x1, y1, x2, y2] -> x1 == x2 or y1 == y2 end)
-      |> Enum.reduce(%{}, &plot/2)
-      |> Enum.count(fn {_, c} -> c >= 2 end)
+      |> Enum.each(&plot(&1, ets))
+
+    count = :ets.select_count(ets, [{{:"$1", :"$2"}, [{:>, :"$2", 1}], [true]}])
+
+    :ets.delete(ets)
+
+    count
   end
 
   @impl Aoc.Solution
   def part2(input) do
-    Enum.reduce(input, %{}, &plot/2)
-      |> Enum.count(fn {_, c} -> c >= 2 end)
+    ets = :ets.new(:state_store, [:set, write_concurrency: true, read_concurrency: true])
+
+    Enum.each(input, &plot(&1, ets))
+
+    count = :ets.select_count(ets, [{{:"$1", :"$2"}, [{:>, :"$2", 1}], [true]}])
+
+    :ets.delete(ets)
+
+    count
   end
 
   defp plot [x, from1y, x, to1y], map do
-    Enum.reduce(from1y..to1y, map, fn y, acc -> Map.update(acc, {x, y}, 1, &(&1 + 1)) end)
+    Enum.each(from1y..to1y, fn y ->
+      :ets.update_counter(map, {x, y}, {2, 1}, {nil, 0})
+    end)
   end
   defp plot [from1x, y, to1x, y], map do
-    Enum.reduce(from1x..to1x, map, fn x, acc -> Map.update(acc, {x, y}, 1, &(&1 + 1)) end)
+    Enum.each(from1x..to1x, fn x ->
+      :ets.update_counter(map, {x, y}, {2, 1}, {nil, 0})
+    end)
   end
   defp plot([from1x, from1y, to1x, to1y], map) when from1x - to1x == from1y - to1y do
     min_x = min(from1x, to1x)
     min_y = min(from1y, to1y)
 
-    Enum.reduce(0..abs(from1x - to1x), map, fn delta, acc ->
-      Map.update(acc, {min_x + delta, min_y + delta}, 1, &(&1 + 1))
+    Enum.each(0..abs(from1x - to1x), fn delta ->
+      :ets.update_counter(map, {min_x + delta, min_y + delta}, {2, 1}, {nil, 0})
     end)
   end
   defp plot([from1x, from1y, to1x, to1y], map) when from1x - to1x == to1y - from1y do
     min_x = min(from1x, to1x)
     max_y = max(from1y, to1y)
 
-    Enum.reduce(0..abs(from1x - to1x), map, fn delta, acc ->
-      Map.update(acc, {min_x + delta, max_y - delta}, 1, &(&1 + 1))
+    Enum.each(0..abs(from1x - to1x), fn delta ->
+      :ets.update_counter(map, {min_x + delta, max_y - delta}, {2, 1}, {nil, 0})
     end)
   end
 end
