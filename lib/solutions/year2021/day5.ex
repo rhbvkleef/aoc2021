6211ca7d144dedb4c3b8fd771207f7964a399735
Rolf  van Kleef <rkleef@iapc.nl>
2021-12-05 15:39:02 +0100

    meta(day5): Use map instead of list of tuples

--- lib/solutions/year2021/day5.ex
+++ lib/solutions/year2021/day5.ex
@@ -11,36 +11,36 @@ aoc 2021, 5 do
   @impl Aoc.Solution
   def part1(input) do
     Enum.filter(input, fn [x1, y1, x2, y2] -> x1 == x2 or y1 == y2 end)
-      |> Enum.flat_map(&pairs/1)
-      |> Enum.frequencies()
-      |> Enum.filter(fn {_, amount} -> amount >= 2  end)
-      |> Enum.count
+      |> Enum.reduce(%{}, &plot/2)
+      |> Enum.count(fn {_, c} -> c >= 2 end)
   end
 
   @impl Aoc.Solution
   def part2(input) do
-    Enum.flat_map(input, &pairs/1)
-      |> Enum.frequencies()
-      |> Enum.filter(fn {_, amount} -> amount >= 2  end)
-      |> Enum.count
+    Enum.reduce(input, %{}, &plot/2)
+      |> Enum.count(fn {_, c} -> c >= 2 end)
   end
 
-  defp pairs [x, from1y, x, to1y] do
-    Enum.map(from1y..to1y, fn y -> {x, y} end)
+  defp plot [x, from1y, x, to1y], map do
+    Enum.reduce(from1y..to1y, map, fn y, acc -> Map.update(acc, {x, y}, 1, &(&1 + 1)) end)
   end
-  defp pairs [from1x, y, to1x, y] do
-    Enum.map(from1x..to1x, fn x -> {x, y} end)
+  defp plot [from1x, y, to1x, y], map do
+    Enum.reduce(from1x..to1x, map, fn x, acc -> Map.update(acc, {x, y}, 1, &(&1 + 1)) end)
   end
-  defp pairs([from1x, from1y, to1x, to1y]) when from1x - to1x == from1y - to1y do
+  defp plot([from1x, from1y, to1x, to1y], map) when from1x - to1x == from1y - to1y do
     min_x = min(from1x, to1x)
     min_y = min(from1y, to1y)
 
-    Enum.map(0..abs(from1x - to1x), fn delta -> {min_x + delta, min_y + delta} end)
+    Enum.reduce(0..abs(from1x - to1x), map, fn delta, acc ->
+      Map.update(acc, {min_x + delta, min_y + delta}, 1, &(&1 + 1))
+    end)
   end
-  defp pairs([from1x, from1y, to1x, to1y]) when from1x - to1x == to1y - from1y do
+  defp plot([from1x, from1y, to1x, to1y], map) when from1x - to1x == to1y - from1y do
     min_x = min(from1x, to1x)
     max_y = max(from1y, to1y)
 
-    Enum.map(0..abs(from1x - to1x), fn delta -> {min_x + delta, max_y - delta} end)
+    Enum.reduce(0..abs(from1x - to1x), map, fn delta, acc ->
+      Map.update(acc, {min_x + delta, max_y - delta}, 1, &(&1 + 1))
+    end)
   end
 end
