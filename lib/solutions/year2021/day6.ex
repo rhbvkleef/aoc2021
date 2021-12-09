6f86120eb868d608380f30ae74ad3f6b3d1c783e
Rolf  van Kleef <rkleef@iapc.nl>
2021-12-09 18:46:44 +0100

    meta(day6): Use Nx.LinAlg.matrix_power

--- lib/solutions/year2021/day6.ex
+++ lib/solutions/year2021/day6.ex
@@ -29,7 +29,7 @@ aoc 2021, 6 do
       1 0 0 0 0 0 0 0 0
       """
 
-    function = dot_power(matrix, remaining_days)
+    function = Nx.LinAlg.matrix_power(matrix, remaining_days)
 
     Nx.dot(function, counters) |> Nx.sum |> Nx.to_number
   end
@@ -37,48 +37,5 @@ aoc 2021, 6 do
   @impl Aoc.Solution
   def part2(input) do
     part1(input, 256)
-  end
-
-  ##
-  # Matrix exponentiation
-  # Source: https://github.com/elixir-nx/nx/pull/583
-  # Author: @rhbvkleef (me)
-  ##
-
-  import Nx.Defn.Kernel, only: [assert_shape_pattern: 2]
-
-  def dot_power(tensor, power) when is_integer(power) and power < 0 do
-    dot_power(Nx.LinAlg.invert(tensor), -power)
-  end
-
-  def dot_power(tensor, 0) do
-    # We need a special-case for 0 since the code below
-    # is optimized to not compute an initial eye.
-    Nx.Defn.Kernel.assert_shape_pattern(tensor, {x, x})
-
-    Nx.eye(tensor)
-  end
-
-  def dot_power(tensor, power) when is_integer(power) do
-    Nx.Defn.Kernel.assert_shape_pattern(tensor, {x, x})
-
-    power
-    |> Integer.digits(2)
-    |> tl()
-    |> Enum.reverse()
-    |> Enum.reduce({nil, tensor}, fn
-      1, {nil, exp_tensor} ->
-        {exp_tensor, Nx.dot(exp_tensor, exp_tensor)}
-
-      1, {result_tensor, exp_tensor} ->
-        {Nx.dot(result_tensor, exp_tensor), Nx.dot(exp_tensor, exp_tensor)}
-
-      0, {result_tensor, exp_tensor} ->
-        {result_tensor, Nx.dot(exp_tensor, exp_tensor)}
-    end)
-    |> then(fn
-      {nil, exp_tensor} -> exp_tensor
-      {result, exp_tensor} -> Nx.dot(result, exp_tensor)
-    end)
   end
 end
